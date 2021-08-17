3d026f37425195e280dd4171f0baff59fa78c60a
sofiary <33208002+sofiary@users.noreply.github.com>
2021-08-17 22:44:41 +0800

    plotly experiments

--- src/simulation.py
+++ src/simulation.py
@@ -1,8 +1,10 @@
 import pandapower as pp
 import pandapower.networks as pn
-import pandapower.plotting as pplt
+from pandapower.plotting.plotly import pf_res_plotly
 
 if __name__ == "__main__":
     net = pn.case118()
     print(net)
-    pplt.simple_plot(net)
+    #pplt.simple_plot(net)
+    pp.runpp(net)
+    pf_res_plotly(net)
