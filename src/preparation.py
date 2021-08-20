97a3c1246ff66be500daf6ac8bc0afc46a485a7c
sofiary <33208002+sofiary@users.noreply.github.com>
2021-08-20 23:00:13 +0800

    extension of builder

--- src/preparation.py
+++ src/preparation.py
@@ -1,9 +1,36 @@
 import json
+from numpy import true_divide
 import pandapower as pp
 import pandas as pd
 
-def network_data_parser(file: str, custom_rules: str) -> pd.DataFrame:
-    pass
+class NetworkBuilder:
 
-def network_creator(df: pd.DataFrame) -> pp.auxiliary.pandapowerNet:
-    pass
+    def __init__(self, *, name: str, f_hz: float, sn_mva: int):
+        self.net = pp.create_empty_network(name=name, f_hz=f_hz, sn_mva=sn_mva)
+
+    def __repr__(self):
+        pass
+
+    def parse_data(self, import_rules) -> pd.DataFrame:
+        with open(import_rules) as ir:
+            json_rules = json.load(ir)
+        assert json_rules is not None, "Failed to load json config."
+        worksheets = json_rules["worksheet_relevancy"]
+        ignored_rows = json_rules["ignored_rows"]
+        self.data = pd.read_excel(f"data/{json_rules['workbook_name']}",
+                      sheet_name=[key for key,val in worksheets.items() if val],
+                      skiprows=lambda x: x+1 in ignored_rows)
+
+    def build_nodes(self):
+        assert self.data is not None, "Please parse necessary data first."
+        for index in (df := self.data["ConnectionPoints"]).index:
+            node_data = {
+                "name": df.loc[index, "Connection Point"],
+                "index": None,
+                "vn_kv": df.loc[index, "Voltage level"],
+                "geodata": (df.loc[index, "Latitude"], df.loc[index, "Longitude"]),
+                "zone": None,
+                "in_service": True,
+            }
+            pp.create_bus(self.net, **node_data)
+
--- src/simulation.py
+++ src/simulation.py
@@ -1,11 +1,16 @@
+import os
 import pandapower as pp
 import pandapower.networks as pn
 from pandapower.plotting.plotly import pf_res_plotly
-#from preparation import 
+from preparation import NetworkBuilder
 
+import_rules = dict()
+import_rules["aemo"] = r"data/aemo_import_rules.json"
+import_rules["electranet"] = r"data/electranet_import_rules.json"
 if __name__ == "__main__":
-    net = pn.case118()
-    print(net.__class__)
-    #pplt.simple_plot(net)
-    pp.runpp(net)
-    pf_res_plotly(net)
+    builder = NetworkBuilder(name="ElectraNet", f_hz=50.0, sn_mva=100)
+    builder.parse_data(import_rules=import_rules["electranet"])
+    builder.build_nodes()
+    breakpoint()
+    print(builder)
+    #pf_res_plotly(net)
